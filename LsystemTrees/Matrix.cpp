ccf3c279815ab66905fce4fa1df07dbaf98f788a
Karishma Vakil <karishmavakil@karishmas-mbp.lan>
2018-01-13 10:56:38 +0000

    renaming Matrix

---
+++ LsystemTrees/Matrix.cpp
@@ -0,0 +1,42 @@
+//
+//  Matrix.cpp
+//  LsystemTrees
+//
+//  Created by Karishma Vakil on 03/01/2018.
+//  Copyright © 2018 Karishma Vakil. All rights reserved.
+//
+
+#include "Matrix.hpp"
+
+#include <stdio.h>
+
+#include "Vector3D.hpp"
+
+// third-party libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// standard C++ libraries
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+
+#include <iostream>
+using namespace std;
+
+
+Matrix::Matrix() {
+    col1 = Vector3D();
+    col2 = Vector3D();
+    col3 = Vector3D();
+}
+void Matrix::printState(){
+    cout<<"col1: ";
+    col1.printState();
+    cout<<"col2: ";
+    col2.printState();
+    cout<<"col3: ";
+    col3.printState();
+    cout<<endl;
+}
