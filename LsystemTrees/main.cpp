40fc727978659ac54d7be2de331c6e9c86f9b22d
Karishma Vakil <karishmavakil@karishmas-mbp.lan>
2018-02-13 09:40:06 +0000

    changing interpreter information, using json reader now

--- LsystemTrees/main.cpp
+++ LsystemTrees/main.cpp
@@ -20,16 +20,16 @@
 #include <fstream>
 #include <cstdlib>
 #include <regex>
+#include <random>
 
 #include "Window.hpp"
 #include "Shaders.hpp"
 #include "Turtle.hpp"
-#include "Reader.hpp"
 #include "Tests.hpp"
 #include "TurtleInterpreter.hpp"
 #include "Controls.hpp"
 #include "Parser.hpp"
-
+#include "JSONReader.hpp"
 
 using namespace std;
 
@@ -37,6 +37,10 @@ using namespace std;
 #include "glm/gtx/transform.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/ext.hpp"
+#include "json.hpp"
+
+// for convenience
+using json = nlohmann::json;
 
 using namespace glm;
 
@@ -61,20 +65,18 @@ int testGraphics() {
 
     GLfloat vertices[1000000] = {};
     GLfloat colours[1000000] = {};
+//    Turtle turtle = Turtle();
+//    turtle.setPosition(0.0f, -6.0f, 0.0f);
+//    
+//    
+//    LSystem tree = createLsystem("/Users/karishmavakil/Documents/Project/LSystemTrees/LSystemTrees/tree.txt");
+//    tree.applyRules(4);
+//    
 
-    Turtle turtle = Turtle();
-    turtle.setPosition(0.0f, -6.0f, 0.0f);
-    
-    
-    LSystem tree = createLsystem("/Users/karishmavakil/Documents/Project/LSystemTrees/LSystemTrees/tree.txt");
-    tree.applyRules(4);
-    
-    
-    TurtleInterpreter interpreter = TurtleInterpreter(turtle);
-    interpreter.readVariables("/Users/karishmavakil/Documents/Project/LSystemTrees/LSystemTrees/turtleinterpreter.txt");
+    TurtleInterpreter interpreter= createInterpreter("/Users/karishmavakil/Documents/Project/LsystemTrees/LsystemTrees/tree1.json");
     interpreter.printVariables();
 
-    interpreter.generateInformation(tree.current);
+    interpreter.generateInformation();
     
     vector<vec3> vert = interpreter.getVertices();
     vector<vec3> col = interpreter.getColours();
@@ -89,17 +91,15 @@ int testGraphics() {
         colours[3*i+1] = itC->y;
         colours[3*i+2] = itC->z;
     }
-    
-    GLuint vertexbuffer, VAO;
-    
+
+    GLuint VAO;
     glGenVertexArrays( 1, &VAO);
-    glGenBuffers(1, &vertexbuffer);
-    
     glBindVertexArray(VAO);
     
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
     glBindBuffer( GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
-    
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 3 * sizeof( GLfloat), (GLvoid *) 0);
     glEnableVertexAttribArray(0);
     glBindBuffer( GL_ARRAY_BUFFER, 0);
@@ -117,7 +117,7 @@ int testGraphics() {
     glfwPollEvents();
 
     while ((!glfwWindowShouldClose(window)) && glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS) {
-        
+
         // Enable depth test
         glEnable(GL_DEPTH_TEST);
         // Accept fragment if it closer to the camera than the former one
@@ -165,11 +165,42 @@ int main () {
 //    testLSystem();
 //    testParametersLSystem();
     testGraphics();
+//    testJSONReader();
 //    testRegexes();
 //    testD0LSystemTurtle();
 //    testReader();
 //    testParser();
 //    testInterpreter();
+//    std::ifstream i("/Users/karishmavakil/Documents/Project/LsystemTrees/LsystemTrees/file.json");
+//    json o;
+//    i >> o;
+//    // special iterator member functions for objects
+//    for (json::iterator it = o.begin(); it != o.end(); ++it) {
+//        if (it.key()=="answer") {
+//            for (json::iterator it2 = it.value().begin(); it2 != it.value().end() ; it2++) {
+//                cout<<it2.key()<<" "<<it2.value();
+//            }
+//        }
+//    }
+//    
+//    // find an entry
+//    if (o.find("foo") != o.end()) {
+//        // there is an entry with key "foo"
+//    }
+//    TurtleInterpreter interpreter= createInterpreter("/Users/karishmavakil/Documents/Project/LsystemTrees/LsystemTrees/file.json");
+//
+//    random_device rd{}; // use to seed the rng
+//    mt19937 rng{rd()}; // rng
+//    
+//    double p = 0.2; // probability
+//    std::bernoulli_distribution d(p);
+//    
+//    // generate 5 runs
+//    for(std::size_t i = 0; i < 50; ++i)
+//        std::cout << d(rng) << " ";
+//    
+//    
+    
 }
 
 
