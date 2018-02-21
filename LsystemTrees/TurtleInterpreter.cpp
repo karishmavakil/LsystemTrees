82ebc8525a8886c99d168ad7df15ec292e7ecb3e
Karishma Vakil <karishmavakil@karishmas-mbp.lan>
2018-02-21 10:33:28 +0000

    adding  leaves and leaf variables

--- LsystemTrees/TurtleInterpreter.cpp
+++ LsystemTrees/TurtleInterpreter.cpp
@@ -48,20 +48,34 @@ TurtleInterpreter::TurtleInterpreter(Turtle t, LSystem l){
     minRadius = 0.03;
     iterations = 5;
     useContexts = false;
+    maxLeafRadius = 0.13;
+    leafLength = 0.6;
+    leafWidth = 0.35;
+    leafDensity = 8;
+    woodTextureFile = "woodtexture.bmp";
+    leafTextureFile = "leaf2.bmp";
 };
 vector<Symbol> TurtleInterpreter::getInstructions(){
     return lsystem.current;
 };
 vector<vec3> TurtleInterpreter::getVertices(){
+    vector<vec3> vertices = branchVertices;
+    vertices.insert(vertices.end(), leafVertices.begin(), leafVertices.end());
     return vertices;
 };
 vector<vec3> TurtleInterpreter::getColours(){
+    vector<vec3> colours = branchColours;
+    colours.insert(colours.end(), leafColours.begin(), leafColours.end());
     return colours;
 };
 vector<vec3> TurtleInterpreter::getNormals(){
+    vector<vec3> normals = branchNormals;
+    normals.insert(normals.end(), leafNormals.begin(), leafNormals.end());
     return normals;
 };
 vector<vec2> TurtleInterpreter::getUVs(){
+    vector<vec2> uvs = branchUVs;
+    uvs.insert(uvs.end(), leafUVs.begin(), leafUVs.end());
     return uvs;
 };
 LSystem TurtleInterpreter::getLSystem(){
@@ -87,7 +101,10 @@ void TurtleInterpreter::generateInformation(){
             case 'F':
                 oldPosition = turtle.getPosition();
                 turtle.forward(p[0]*branchStep);
-                addCylinderVertices(oldPosition, turtle.getPosition(), turtle.getHeading(), turtle.getLeft(), branchRadius);
+                addBranchVertices(oldPosition, turtle.getPosition(), turtle.getHeading(), turtle.getLeft(), branchRadius);
+                if(branchRadius <= maxLeafRadius){
+                    addLeavesVertices(oldPosition, turtle.getPosition(), turtle.getHeading(), turtle.getLeft(), branchRadius);
+                }
                 //                vertices.push_back(oldPos);
                 //                vertices.push_back(position);
                 if (branchRadius > minRadius){
@@ -142,6 +159,8 @@ void TurtleInterpreter::generateInformation(){
                 break;
         }
     }
+    branchVerticesSize = branchVertices.size();
+    leafVerticesSize = leafVertices.size();
 }
 //#declare DarkBrown = color red 0.36 green 0.25 blue 0.20
 //#declare DarkTan = color red 0.59 green 0.41 blue 0.31
@@ -149,64 +168,105 @@ void TurtleInterpreter::generateInformation(){
 //#declare MediumWood = color red 0.65 green 0.50 blue 0.39
 //#declare LightWood = color red 0.91 green 0.76 blue 0.65
 
-void TurtleInterpreter::addCylinderVertices (vec3 centre1, vec3 centre2, vec3 axis, vec3 perp, GLfloat radius) {
-    vector<vec3> cylinderVertices;
-    vector<vec3> cylinderColours;
-    vector<vec3> cylinderNormals;
-    vector<vec2> cylinderUVs;
+void TurtleInterpreter::addBranchVertices (vec3 centre1, vec3 centre2, vec3 axis, vec3 perp, GLfloat radius) {
     vec4 point1 = radius * vec4(normalize(perp), 0);
-    vec4 point2 = radius * 0.75f * vec4(normalize(perp), 0);
+    vec4 point2 = radius * 0.8f * vec4(normalize(perp), 0);
     vec3 side1, side2;
     vec3 normal;
-    float n = 11;
+    float n = 8;
     mat4 rotation = rotate(mat4(1.0f),(float) (2 * M_PI )/ n, normalize(axis));
     for(int i = 0; i < n; i++){
         
-        cylinderVertices.push_back(vec3(point1) + centre1);
-        cylinderUVs.push_back(vec2(1, i/n));
-        cylinderColours.push_back(vec3(0.36, 0.25, 0.2));
-        cylinderVertices.push_back(vec3(point2) + centre2);
-        cylinderUVs.push_back(vec2(0, i/n));
+        branchVertices.push_back(vec3(point1) + centre1);
+        branchUVs.push_back(vec2(1, i/n));
+        branchColours.push_back(vec3(0.36, 0.25, 0.2));
+        branchVertices.push_back(vec3(point2) + centre2);
+        branchUVs.push_back(vec2(0, i/n));
 
-        cylinderColours.push_back(vec3(0.59, 0.41, 0.31));
+        branchColours.push_back(vec3(0.59, 0.41, 0.31));
         side1 = vec3(point1) + centre1 - vec3(point2) - centre2;
         vec3 prev = vec3(point2) + centre2;
         point1 = point1 * rotation;
         point2 = point2 * rotation;
-        cylinderVertices.push_back(vec3(point1) + centre1);
-        cylinderUVs.push_back(vec2(1, (i+1)/n));
+        branchVertices.push_back(vec3(point1) + centre1);
+        branchUVs.push_back(vec2(1, (i+1)/n));
 
         side2 = - prev + vec3(point1) + centre1;
         normal = cross(side1, side2);
-        cylinderNormals.push_back(normal);
-        cylinderNormals.push_back(normal);
-        cylinderNormals.push_back(normal);
-        cylinderColours.push_back(vec3(0.55, 0.47, 0.14));
-        cylinderVertices.push_back(prev);
-        cylinderUVs.push_back(vec2(0, i/n));
+        branchNormals.push_back(normal);
+        branchNormals.push_back(normal);
+        branchNormals.push_back(normal);
+        branchColours.push_back(vec3(0.55, 0.47, 0.14));
+        branchVertices.push_back(prev);
+        branchUVs.push_back(vec2(0, i/n));
 
-        cylinderColours.push_back(vec3(0.65, 0.5, 0.39));
-        cylinderVertices.push_back(vec3(point1) + centre1);
-        cylinderUVs.push_back(vec2(1, (i+1)/n));
+        branchColours.push_back(vec3(0.65, 0.5, 0.39));
+        branchVertices.push_back(vec3(point1) + centre1);
+        branchUVs.push_back(vec2(1, (i+1)/n));
 
         side1 = vec3(point2) + centre2 - vec3(point1) - centre1;
         normal = cross(side2, side1);
-        cylinderNormals.push_back(normal);
-        cylinderNormals.push_back(normal);
-        cylinderNormals.push_back(normal);
-        cylinderColours.push_back(vec3(0.91, 0.76, 0.65));
-        cylinderVertices.push_back(vec3(point2) + centre2);
-        cylinderUVs.push_back(vec2(0, (i+1)/n));
-        cylinderColours.push_back(vec3(0.36, 0.25, 0.2));
+        branchNormals.push_back(normal);
+        branchNormals.push_back(normal);
+        branchNormals.push_back(normal);
+        branchColours.push_back(vec3(0.91, 0.76, 0.65));
+        branchVertices.push_back(vec3(point2) + centre2);
+        branchUVs.push_back(vec2(0, (i+1)/n));
+        branchColours.push_back(vec3(0.36, 0.25, 0.2));
 
     }
-    vertices.insert(vertices.end(), cylinderVertices.begin(), cylinderVertices.end());
-    colours.insert(colours.end(), cylinderColours.begin(), cylinderColours.end());
-    normals.insert(normals.end(), cylinderNormals.begin(), cylinderNormals.end());
-    uvs.insert(uvs.end(), cylinderUVs.begin(), cylinderUVs.end());
 }
 
-void TurtleInterpreter::addLeafVertices(vec3 end, vec3 dir, GLfloat length) {
+void TurtleInterpreter::addLeafVertices(vec3 end, vec3 dir, vec3 perp) {
+    GLfloat width = leafWidth;
+    GLfloat length = leafLength;
+    vec3 normal = cross(dir, perp);
+    vec3 outer = end + length * dir;
+    vec3 left = end + length/3 * dir + width / 2 * perp + 0.08 * normal;
+    vec3 right = end + length/3 * dir - width / 2 * perp + 0.08 * normal;
+    vec3 side1 = left - end;
+    vec3 side2 = right - end;
+    leafVertices.push_back(end);
+    leafUVs.push_back(vec2(1, 0.5));
+    leafVertices.push_back(left);
+    leafUVs.push_back(vec2(0.7, 0));
+    leafVertices.push_back(outer);
+    leafUVs.push_back(vec2(0, 0.5));
+    leafVertices.push_back(end);
+    leafUVs.push_back(vec2(1, 0.5));
+    leafVertices.push_back(right);
+    leafUVs.push_back(vec2(0.7, 1));
+    leafVertices.push_back(outer);
+    leafUVs.push_back(vec2(0, 0.5));
+
+    leafColours.push_back(vec3(0.00, 0.76, 0.30));
+    leafColours.push_back(vec3(0.00, 0.76, 0.40));
+    leafColours.push_back(vec3(0.00, 0.76, 0.30));
+    leafColours.push_back(vec3(0.00, 0.76, 0.40));
+    leafColours.push_back(vec3(0.00, 0.76, 0.30));
+    leafColours.push_back(vec3(0.00, 0.76, 0.40));
+    normal = cross(dir, side1);
+    leafNormals.push_back(normal);
+    leafNormals.push_back(normal);
+    leafNormals.push_back(normal);
+    normal = cross(side2, dir);
+    leafNormals.push_back(normal);
+    leafNormals.push_back(normal);
+    leafNormals.push_back(normal);
+    
+}
+void TurtleInterpreter::addLeavesVertices(vec3 centre1, vec3 centre2, vec3 axis, vec3 perp, GLfloat radius) {
+//    GLfloat ratio = 0.8f;
+    vec4 endDir = radius * vec4(normalize(perp), 0);
+    vec3 normal;
+    GLfloat n = (float) leafDensity;
+    mat4 rotation = rotate(mat4(1.0f),(float) (2 * M_PI )/ n, normalize(axis));
+    for(int i = 0; i < n; i++){
+        endDir = endDir * rotation;
+//        vec3 leafEnd = centre1 + (i/n) * (centre2 - centre1) + ((i/n) * ratio + (n - i/n)) * vec3(endDir);
+        vec3 leafEnd = centre1 + (i/n) * (centre2 - centre1) + vec3(endDir);
+        addLeafVertices(leafEnd, normalize(endDir), axis);
+    }
     
 }
 void TurtleInterpreter::printVariables() {
@@ -216,4 +276,12 @@ void TurtleInterpreter::printVariables() {
     cout<<"branchThicknessRatio "<<branchThicknessRatio<<endl;
     cout<<"branchStepRatio "<<branchStepRatio<<endl;
     cout<<"minRadius "<<minRadius<<endl;
+    cout<<"maxLeafRadius "<<maxLeafRadius<<endl;
+    cout<<"leafLength "<<leafLength<<endl;
+    cout<<"leafWidth "<<leafWidth<<endl;
+    cout<<"leafDensity "<<leafDensity<<endl;
+    cout<<"woodTextureFile "<<woodTextureFile<<endl;
+    cout<<"leafTextureFile "<<leafTextureFile<<endl;
+    cout<<"leafStyle "<<endl;
+
 }
